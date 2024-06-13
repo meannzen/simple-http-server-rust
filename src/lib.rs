607f5f271ec0bee57d5119716074f45ba7b80af3
senmeannrealwat <meann.sen@realwat.net>
2024-06-13 02:51:20 -0600

    codecrafters submit [skip ci]

---
+++ src/lib.rs
@@ -0,0 +1,207 @@
+use std::{
+    collections::HashMap,
+    io::{BufRead, BufReader, Read, Write},
+    net::TcpStream,
+    str::FromStr,
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct ParseSteamError(String);
+
+#[derive(Default, PartialEq, Debug, Clone, Copy, Eq)]
+pub enum StatusCode {
+    #[default]
+    OK,
+    Created,
+    NotFound,
+}
+
+impl std::fmt::Display for StatusCode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let status = match &self {
+            StatusCode::OK => "200 OK",
+            StatusCode::Created => "201 Created",
+            _ => "404 Not Found",
+        };
+        write!(f, "{status}")
+    }
+}
+#[derive(Debug, Default, Clone)]
+pub struct Response {
+    status: StatusCode,
+    header: HashMap<String, String>,
+    body: Vec<u8>,
+}
+impl Response {
+    pub fn ok() -> Response {
+        Response {
+            status: StatusCode::OK,
+            ..Default::default()
+        }
+    }
+
+    pub fn not_found() -> Response {
+        Response {
+            status: StatusCode::NotFound,
+            ..Default::default()
+        }
+    }
+
+    pub fn created() -> Response {
+        Response {
+            status: StatusCode::Created,
+            ..Default::default()
+        }
+    }
+
+    pub fn set_header(mut self, key: &str, value: &str) -> Self {
+        self.header
+            .entry(key.to_owned())
+            .or_insert(value.to_owned());
+        self
+    }
+
+    pub fn set_body(mut self, body: impl AsRef<[u8]>) -> Self {
+        self.body.extend_from_slice(body.as_ref());
+        self
+    }
+
+    pub fn write(self, mut writer: impl Write) -> Result<(), std::io::Error> {
+        let status_line = format!("HTTP/1.1 {}\r\n", self.status);
+        writer.write_all(status_line.as_bytes())?;
+        for (k, v) in self.header.into_iter() {
+            writer.write_all(format!("{k}: {v}\r\n").as_bytes())?;
+        }
+
+        writer.write_all(b"\r\n")?;
+        // body
+        writer.write_all(&self.body)?;
+
+        Ok(())
+    }
+}
+impl FromStr for Method {
+    type Err = ParseSteamError;
+    fn from_str(method: &str) -> Result<Self, Self::Err> {
+        match method {
+            "GET" => Ok(Method::GET),
+            "POST" => Ok(Method::POST),
+            _ => Err(ParseSteamError(format!("Invalid Http Method {}", method))),
+        }
+    }
+}
+
+pub struct ThreadPool {
+    pub workers: Vec<Worker>,
+    pub sender: mpsc::Sender<Job>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+impl ThreadPool {
+    pub fn new(size: usize) -> Self {
+        assert!(size > 0); // panic
+
+        let mut workers = Vec::with_capacity(size);
+        let (sender, receiver) = mpsc::channel::<Job>();
+        let receiver = Arc::new(Mutex::new(receiver));
+        for id in 0..size {
+            let worker = Worker::new(id, Arc::clone(&receiver));
+            workers.push(worker);
+        }
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
+}
+
+#[allow(unused)]
+pub struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        let thread = thread::spawn(move || {
+            while let Ok(job) = receiver.lock().unwrap().recv() {
+                println!("Worker {id} got a job; executing.");
+
+                job();
+            }
+        });
+
+        Self { id, thread }
+    }
+}
+
+#[derive(Debug, Default)]
+pub enum Method {
+    #[default]
+    GET,
+    POST,
+}
+
+#[derive(Debug, Default)]
+pub struct Request {
+    pub method: Method,
+    pub path: String,
+    pub header: HashMap<String, String>,
+    pub body: Vec<u8>,
+}
+
+pub fn parse_request(mut stream: &mut TcpStream) -> Result<Request, ParseSteamError> {
+    let mut buf_reader = BufReader::new(&mut stream);
+    let mut http_request: Vec<String> = vec![];
+
+    for line in buf_reader.by_ref().lines() {
+        let line = line.map_err(|e| ParseSteamError(e.to_string()))?;
+        if line.is_empty() {
+            break;
+        }
+        http_request.push(line);
+    }
+
+    let parts: Vec<_> = http_request[0].split(' ').collect();
+    if parts.len() != 3 {
+        return Err(ParseSteamError("Invalid request".to_string()));
+    }
+
+    let method: Method = parts[0].parse()?;
+    let path = parts[1].to_string();
+    let header: HashMap<String, String> = http_request[1..]
+        .iter()
+        .filter_map(|line| {
+            line.split_once(": ")
+                .map(|(k, v)| (k.to_owned(), v.to_owned()))
+        })
+        .collect();
+
+    let mut body = Vec::new();
+    if let Some(length) = header.get("Content-Length") {
+        match length.parse() {
+            Ok(n) => {
+                body.resize(n, 0);
+                buf_reader
+                    .read_exact(&mut body)
+                    .map_err(|e| ParseSteamError(e.to_string()))?;
+            }
+            _ => {}
+        };
+    }
+
+    Ok(Request {
+        method,
+        path,
+        header,
+        body,
+    })
+}
--- src/main.rs
+++ src/main.rs
@@ -1,145 +1,109 @@
+use core::panic;
+use http_server_starter_rust::{parse_request, Method, Request, Response, ThreadPool};
 use std::{
-    collections::HashMap,
     env,
     fs::File,
     io::{Read, Write},
-    net::TcpStream,
-    thread,
+    net::{TcpListener, TcpStream},
 };
 
-#[derive(Debug)]
-struct HttpRequst {
-    method: String,
-    path: String,
-    version: String,
-    header: HashMap<String, String>,
-}
-
-impl HttpRequst {
-    fn new() -> Self {
-        HttpRequst {
-            method: String::new(),
-            path: String::new(),
-            version: String::new(),
-            header: HashMap::new(),
-        }
-    }
-
-    fn from_raw(raw_data: Vec<&str>) -> Self {
-        let mut request = HttpRequst::new();
-        if raw_data.len() < 3 {
-            return request;
-        }
-        request.method = raw_data[0].to_string();
-        request.path = raw_data[1].to_string();
-        request.version = raw_data[2].to_string();
-        let mut i = 3;
-        while i < raw_data.len() {
-            if i + 1 < raw_data.len() {
-                let key = raw_data[i].trim_end_matches(':').to_string();
-                let value = raw_data[i + 1].to_string();
-                request.header.entry(key).or_insert(value);
-            }
-            i += 2;
-        }
-
-        request
-    }
-}
-
-fn read_stream(mut stream: &TcpStream) -> Option<HttpRequst> {
-    let mut buffer = [0; 1025];
-    let read_result = stream.read(&mut buffer);
-    match read_result {
-        Ok(bytes_read) => {
-            let cow_buff = String::from_utf8_lossy(&buffer[..bytes_read]);
-            let raw_data: Vec<&str> = cow_buff.split_whitespace().collect();
-            let http_request = HttpRequst::from_raw(raw_data);
-            println!("result:{:?}", http_request);
-            Some(http_request)
-        }
-        _ => None,
-    }
-}
-
-fn file_handler(file_name: String) -> String {
-    let path = env::args().nth(2).unwrap();
-    let file_path = format!("{path}/{file_name}");
-    let open_file = File::open(&file_path);
-    match open_file {
-        Ok(mut file) => {
-            let mut content = String::new();
-            match file.read_to_string(&mut content) {
-                Ok(_) => {
-                    return format!("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}\r\n", content.len(), content);
-                }
-                Err(_) => {}
-            }
-        }
-        Err(_) => {}
-    }
-    "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
-}
-
-fn handle_client(stream: &mut TcpStream) -> anyhow::Result<()> {
-    let http_request_option = read_stream(stream);
-    match http_request_option {
-        Some(http_request) => match http_request.path.as_str() {
-            "/" => {
-                let _ = stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes());
-            }
-            path => {
-                if path == "/echo" || path.starts_with("/echo/") {
-                    if path == "/echo" {
-                        let _ = stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes());
-                    } else if let Some(content) = path.strip_prefix("/echo/") {
-                        let content_lenght = content.len();
-                        let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", content_lenght, content);
-                        let _ = stream.write(response.as_bytes());
-                    } else {
-                        let _ = stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes());
-                    }
-                } else if path == "/user-agent" || path == "/user-agent/" {
-                    if let Some(content) = http_request.header.get("User-Agent") {
-                        let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", content.len(), content);
-                        let _ = stream.write(response.as_bytes());
-                    } else {
-                        let _ = stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes());
-                    }
-                } else if path.starts_with("/files/") {
-                    if let Some(file_name) = path.strip_prefix("/files/") {
-                        let response = file_handler(file_name.to_string());
-                        let _ = stream.write(response.as_bytes());
-                    } else {
-                        let _ = stream.write("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes());
-                    }
-                } else {
-                    let _ = stream.write("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes());
-                }
-            }
-        },
-        None => {
-            let _ = stream.write("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes());
-        }
-    }
-
-    Ok(())
-}
-
-fn main() -> std::io::Result<()> {
-    let listener = std::net::TcpListener::bind("127.0.0.1:4221")?;
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    let pool = ThreadPool::new(4);
     for stream in listener.incoming() {
-        thread::spawn(|| match stream {
-            Ok(mut s) => {
-                let _ = handle_client(&mut s);
-            }
-            Err(e) => {
-                println!("Cannot handle request:{:?}", e);
-            }
-        })
-        .join()
-        .unwrap()
+        let stream = stream.unwrap();
+        pool.execute(|| handle_connection(stream));
     }
+}
 
-    Ok(())
+fn handle_connection(mut stream: TcpStream) {
+    let request = match parse_request(&mut stream) {
+        Ok(r) => r,
+        Err(_) => Default::default(),
+    };
+
+    match handle_request(request) {
+        Ok(response) => {
+            let _ = response.write(stream);
+        }
+        Err(_) => {
+            panic!("Server error")
+        }
+    }
+}
+
+fn handle_request(request: Request) -> std::io::Result<Response> {
+    let response = if &request.path == "/" {
+        Response::ok()
+    } else if let Some(content) = request.path.strip_prefix("/echo/") {
+        Response::ok()
+            .set_header("Content-Type", "text/plain")
+            .set_header("Content-Length", content.len().to_string().as_str())
+            .set_body(content.as_bytes())
+    } else if &request.path == "/user-agent" {
+        let user_agent = match request.header.get("User-Agent") {
+            Some(value) => value.to_owned(),
+            None => "".to_owned(),
+        };
+
+        Response::ok()
+            .set_header("Content-type", "text/plain")
+            .set_header("Content-Length", user_agent.len().to_string().as_str())
+            .set_body(user_agent.as_bytes())
+    } else if let Some(file_name) = request.path.strip_prefix("/files/") {
+        let dir = match env::args().nth(2) {
+            Some(path) => path,
+            _ => "/".to_string(),
+        };
+
+        let file_path = format!("{dir}/{file_name}");
+
+        let response = match request.method {
+            Method::GET => {
+                let content = match File::open(&file_path) {
+                    Ok(mut file) => {
+                        let mut buf = String::new();
+                        match file.read_to_string(&mut buf) {
+                            Ok(_) => Some(buf),
+                            Err(_) => None,
+                        }
+                    }
+                    Err(_) => None,
+                };
+
+                let response = match content {
+                    Some(text) => Response::ok()
+                        .set_header("Content-Type", "application/octet-stream")
+                        .set_header("Content-Length", text.len().to_string().as_str())
+                        .set_body(text.as_bytes()),
+                    None => Response::not_found(),
+                };
+
+                response
+            }
+            Method::POST => {
+                let result = match File::create(&file_path) {
+                    Ok(mut file) => {
+                        let writen = match file.write_all(&request.body) {
+                            Ok(_) => Ok(()),
+                            Err(_) => Err(()),
+                        };
+                        writen
+                    }
+                    Err(_) => Err(()),
+                };
+                let response = match result {
+                    Ok(_) => Response::created(),
+                    _ => Response::not_found(),
+                };
+                response
+            }
+        };
+
+        response
+    } else {
+        Response::not_found()
+    };
+
+    Ok(response)
 }
